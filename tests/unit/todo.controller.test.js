69be132ad262f22f58cbf1f9026054391f67bf5c
mcjovial <emmanuelelias455@gmail.com>
2022-06-27 15:12:07 +0100

    test with mock Request object and making it pass!

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,14 +1,27 @@
+const httpMocks = require("node-mocks-http");
 const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../models/todo.model");
+const newTodo = require("../mock-data/new-todo.json");
 
 TodoModel.create = jest.fn();
 
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 describe("TodoController.createTodo", () => {
+  beforeEach(() => {
+    req.body = newTodo;
+  });
+  
   it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
   });
   it("should call TodoModel.create", () => {
-    TodoController.createTodo();
-    expect(TodoModel.create).toBeCalled();
+    TodoController.createTodo(req, res, next);
+    expect(TodoModel.create).toBeCalledWith(newTodo);
   });
 });
